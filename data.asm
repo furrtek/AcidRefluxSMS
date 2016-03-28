f56264bb7d138f1a85e90ec7dd4d97531eb73c9b
Furrtek <furrtek@gmail.com>
2016-03-28 18:46:52 +0200

    Data folder

--- data.asm
+++ data.asm
@@ -2,7 +2,7 @@
 ;  .db 0 	;EOL
 
 tiles_font:
-.include "gfx/tiles_font_score.inc"
+.include "data/tiles_font_score.inc"
 
 tile_empty:
   .db $00 $00 $FF $00 $00 $00 $FF $00
@@ -17,41 +17,41 @@ tile_black:
   .db $FF $FF $FF $FF $FF $FF $FF $FF
 
 tiles_player:
-.include "gfx/tiles_player0.inc"
-.include "gfx/tiles_player1.inc"
-.include "gfx/tiles_player2.inc"
-.include "gfx/tiles_player3.inc"
-.include "gfx/tiles_player4.inc"
-.include "gfx/tiles_player5.inc"
-.include "gfx/tiles_player6.inc"
-.include "gfx/tiles_player7.inc"
+.include "data/tiles_player0.inc"
+.include "data/tiles_player1.inc"
+.include "data/tiles_player2.inc"
+.include "data/tiles_player3.inc"
+.include "data/tiles_player4.inc"
+.include "data/tiles_player5.inc"
+.include "data/tiles_player6.inc"
+.include "data/tiles_player7.inc"
 
 ;tiles_barbpm:
-;.include "gfx/tiles_barbpm.inc"
+;.include "data/tiles_barbpm.inc"
 
 ;tiles_cloud:
-;.include "gfx/tiles_cloud.inc"
+;.include "data/tiles_cloud.inc"
 ;tiles_faces:
-;.include "gfx/tiles_faces.inc"
+;.include "data/tiles_faces.inc"
 ;tiles_blow:
-;.include "gfx/tiles_blow.inc"
+;.include "data/tiles_blow.inc"
 
 tiles_title:
-.include "gfx/tiles_title.inc"
+.include "data/tiles_title.inc"
 map_title:
-.include "gfx/map_title.inc"
+.include "data/map_title.inc"
 
 tiles_gameover:
-.include "gfx/tiles_gameover.inc"
+.include "data/tiles_gameover.inc"
 map_gameover:
-.include "gfx/map_gameover.inc"
+.include "data/map_gameover.inc"
 
 tiles_cloud1:
-.include "gfx/tiles_cloud1.inc"
+.include "data/tiles_cloud1.inc"
 tiles_cloud2:
-.include "gfx/tiles_cloud2.inc"
+.include "data/tiles_cloud2.inc"
 tiles_cloud3:
-.include "gfx/tiles_cloud3.inc"
+.include "data/tiles_cloud3.inc"
 
 map_cloud1:
   .db 0,0,1,0,2,0,3,0,4,0
@@ -61,10 +61,10 @@ map_cloud23:
   .db 6,0,7,0,8,0,9,0,10,0,11,0
 
 tiles_tileset:
-.include "gfx/tiles_tileset.inc"
+.include "data/tiles_tileset.inc"
 
 tiles_objects:
-.include "gfx/tiles_objects.inc"
+.include "data/tiles_objects.inc"
 
 pal_title:
   .db $00 $14 $38 $2C $3E $01 $02 $07 $1B $0F $10 $21 $32
